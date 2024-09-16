2d17b68a0153c643472c21a971d16f2edd85b25e
solov81yova <solov81yova@gmail.com>
2024-09-16 13:40:20 +0200

    add homework_02

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Lesson_02 from './lessons/Lesson_02/Lesson_02';
+import Homework_02 from "./homeworks/homework_02/Homework_02";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   <Lesson_02 />
+   <Homework_02 />
     </div>
   );
 }
